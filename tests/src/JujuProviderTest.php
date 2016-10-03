d46b9a4ef0cb227a1e6ce9a1de5340ac624038f3
Karl <khughes.me@gmail.com>
2016-10-03 18:15:55 -0500

    Updating provider, test

--- tests/src/JujuProviderTest.php
+++ tests/src/JujuProviderTest.php
@@ -1,228 +1,123 @@
 <?php namespace JobApis\Jobs\Client\Providers\Test;
 
+use JobApis\Jobs\Client\Collection;
+use JobApis\Jobs\Client\Job;
 use JobApis\Jobs\Client\Providers\JujuProvider;
+use JobApis\Jobs\Client\Queries\JujuQuery;
 use Mockery as m;
 
 class JujuProviderTest extends \PHPUnit_Framework_TestCase
 {
-    private $clientClass = 'JobBrander\Jobs\Client\Providers\AbstractProvider';
-    private $collectionClass = 'JobBrander\Jobs\Client\Collection';
-    private $jobClass = 'JobBrander\Jobs\Client\Job';
-
     public function setUp()
     {
-        $this->params = [
-            'partnerid' => 'XXXX'
+        $_SERVER['HTTP_USER_AGENT'] = uniqid();
+        $_SERVER['REMOTE_ADDR'] = uniqid();
+
+        $this->query = m::mock('JobApis\Jobs\Client\Queries\JujuQuery');
+
+        $this->client = new JujuProvider($this->query);
+    }
+
+    public function testItCanGetDefaultResponseFields()
+    {
+        $fields = [
+            'title',
+            'company',
+            'city',
+            'state',
+            'country',
+            'source',
+            'link',
+            'onclick',
+            'guid',
+            'postdate',
+            'description',
         ];
-        $this->client = new JujuProvider($this->params);
+        $this->assertEquals($fields, $this->client->getDefaultResponseFields());
     }
 
-    public function testItWillUseXmlFormat()
+    public function testItCanGetListingsPath()
     {
-        $format = $this->client->getFormat();
-
-        $this->assertEquals('xml', $format);
+        $this->assertEquals('channel.item', $this->client->getListingsPath());
     }
 
-    public function testItWillUseGetHttpVerb()
-    {
-        $verb = $this->client->getVerb();
-
-        $this->assertEquals('GET', $verb);
-    }
-
-    public function testListingPath()
-    {
-        $path = $this->client->getListingsPath();
-
-        $this->assertEquals('channel.item', $path);
-    }
-
-    public function testUrlIncludesKeywordWhenProvided()
-    {
-        $keyword = uniqid().' '.uniqid();
-        $param = 'k='.urlencode($keyword);
-
-        $url = $this->client->setKeyword($keyword)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlNotIncludesKeywordWhenNotProvided()
-    {
-        $param = 'k=';
-
-        $url = $this->client->getUrl();
-
-        $this->assertNotContains($param, $url);
-    }
-
-    public function testUrlIncludesLocationWhenCityStateProvided()
-    {
-        $city = uniqid();
-        $state = uniqid();
-        $param = 'l='.urlencode($city.', '.$state);
-
-        $url = $this->client->setLocation($city.', '.$state)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlIncludesLocationWhenCityProvided()
-    {
-        $city = uniqid();
-        $param = 'l='.urlencode($city);
-
-        $url = $this->client->setLocation(urlencode($city))->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlIncludesLocationWhenStateProvided()
-    {
-        $state = uniqid();
-        $param = 'l='.urlencode($state);
-
-        $url = $this->client->setLocation(urlencode($state))->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlNotIncludesLocationWhenNotProvided()
-    {
-        $param = 'l=';
-
-        $url = $this->client->getUrl();
-
-        $this->assertNotContains($param, $url);
-    }
-
-    public function testUrlIncludesCountWhenProvided()
-    {
-        $count = uniqid();
-        $param = 'jpp='.$count;
-
-        $url = $this->client->setCount($count)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlNotIncludesCountWhenNotProvided()
-    {
-        $param = 'jpp=';
-
-        $url = $this->client->setCount(null)->getUrl();
-
-        $this->assertNotContains($param, $url);
-    }
-
-    public function testUrlIncludesDeveloperKeyWhenProvided()
-    {
-        $param = 'partnerid='.$this->params['partnerid'];
-
-        $url = $this->client->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlIncludesPageWhenProvided()
-    {
-        $page = uniqid();
-        $param = 'page='.$page;
-
-        $url = $this->client->setPage($page)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlNotIncludesPageWhenNotProvided()
-    {
-        $param = 'page=';
-
-        $url = $this->client->setPage(null)->getUrl();
-
-        $this->assertNotContains($param, $url);
-    }
-
-    public function testUrlIncludesIpWhenProvided()
-    {
-        $ip = uniqid();
-        $param = 'ipaddress='.$ip;
-
-        $url = $this->client->setIpaddress($ip)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlIncludesIpWhenNotProvided()
-    {
-        $param = 'ipaddress=';
-
-        $url = $this->client->setIpaddress(null)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testUrlIncludesHighlightWhenProvided()
-    {
-        $highlight = uniqid();
-        $param = 'highlight='.$highlight;
-
-        $url = $this->client->setHighlight($highlight)->getUrl();
-
-        $this->assertContains($param, $url);
-    }
-
-    public function testItWillProvideEmptyParameters()
-    {
-        $parameters = $this->client->getParameters();
-
-        $this->assertEmpty($parameters);
-        $this->assertTrue(is_array($parameters));
-    }
-
-    public function testItCanCreateJobFromPayload()
+    public function testItCanCreateJobObjectFromPayload()
     {
         $payload = $this->createJobArray();
+
         $results = $this->client->createJobObject($payload);
 
-        $this->assertEquals($payload['title'], $results->title);
-        $this->assertEquals($payload['company'], $results->company);
-        $this->assertEquals($payload['description'], $results->description);
-        $this->assertEquals($payload['link'], $results->url);
+        $this->assertInstanceOf(Job::class, $results);
+        $this->assertEquals($payload['title'], $results->getTitle());
+        $this->assertEquals($payload['title'], $results->getName());
+        $this->assertEquals($payload['description'], $results->getDescription());
+        $this->assertEquals($payload['company'], $results->getCompanyName());
+        $this->assertEquals($payload['link'], $results->getUrl());
     }
 
-    public function testItCanConnect()
+    /**
+     * Integration test for the client's getJobs() method.
+     */
+    public function testItCanGetJobs()
     {
-        $keyword = uniqid();
-        $responseBody = $this->createXmlResponse();
+        $options = [
+            'k' => uniqid(),
+            'l' => uniqid(),
+            'partnerid' => uniqid(),
+        ];
 
-        $job = m::mock($this->jobClass);
-        $job->shouldReceive('setQuery')
-            ->with($keyword)
-            ->once()
-            ->andReturnSelf();
-        $job->shouldReceive('setSource')
-            ->with('Juju')
-            ->once()
-            ->andReturnSelf();
+        $guzzle = m::mock('GuzzleHttp\Client');
+
+        $query = new JujuQuery($options);
+
+        $client = new JujuProvider($query);
+
+        $client->setClient($guzzle);
 
         $response = m::mock('GuzzleHttp\Message\Response');
-        $response->shouldReceive('getBody')->once()->andReturn($responseBody);
 
-        $http = m::mock('GuzzleHttp\Client');
+        $jobs = $this->createXmlResponse();
 
-        $this->client->setKeyword($keyword);
-
-        $http->shouldReceive(strtolower($this->client->getVerb()))
-            ->with($this->client->getUrl(), $this->client->getHttpClientOptions())
+        $guzzle->shouldReceive('get')
+            ->with($query->getUrl(), [])
             ->once()
             ->andReturn($response);
-        $this->client->setClient($http);
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn($jobs);
 
-        $results = $this->client->getJobs();
+        $results = $client->getJobs();
 
-        $this->assertInstanceOf($this->collectionClass, $results);
+        $this->assertInstanceOf(Collection::class, $results);
+        $this->assertCount(2, $results);
+    }
+
+    /**
+     * Integration test with actual API call to the provider.
+     */
+    public function testItCanGetJobsFromApi()
+    {
+        if (!getenv('PARTNER_ID')) {
+            $this->markTestSkipped('PARTNER_ID not set. Real API call will not be made.');
+        }
+
+        $keyword = 'engineering';
+
+        $query = new JujuQuery([
+            'k' => $keyword,
+            'partnerid' => getenv('PARTNER_ID'),
+            'ipaddress' => getHostByName(getHostName()),
+        ]);
+
+        $client = new JujuProvider($query);
+
+        $results = $client->getJobs();
+
+        $this->assertInstanceOf('JobApis\Jobs\Client\Collection', $results);
+
+        foreach($results as $job) {
+            $this->assertEquals($keyword, $job->query);
+        }
     }
 
     private function createJobArray()
@@ -233,19 +128,17 @@ class JujuProviderTest extends \PHPUnit_Framework_TestCase
             'description' => uniqid(),
             'link' => uniqid(),
             'postdate' => '2015-07-'.rand(1,31),
+            'city' => uniqid(),
+            'state' => uniqid(),
+            'country' => uniqid(),
+            'source' => uniqid(),
+            'onclick' => uniqid(),
+            'guid' => uniqid(),
         ];
     }
 
     private function createXmlResponse()
     {
-        $array = [
-            'channel' => [
-                'item' => [
-                    0 => $this->createJobArray(),
-                    1 => $this->createJobArray()
-                ]
-            ]
-        ];
         $xml = '<?xml version="1.0" encoding="UTF-8"?>
             <rss version="2.0">
                 <channel>
